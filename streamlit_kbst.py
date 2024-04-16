221444dad20b3b9e589d44c72e80347ad0eb2d69
Rizky Yaomal Malik <2010512081@mahasiswa.upnvj.ac.id>
2024-04-16 13:39:49 +0700

    Update streamlit_kbst.py

--- streamlit_kbst.py
+++ streamlit_kbst.py
@@ -27,7 +27,7 @@ if uploaded_file is not None:
         kbst_prediction = kbst_model.predict(df)
 
         # Menyimpan hasil prediksi ke dalam DataFrame hasil
-        hasil = pd.DataFrame(kbst_prediction, columns=['Hasil Prediksi'])
+        hasil = pd.DataFrame(kbst_prediction, columns=['hasil prediksi'])
 
         # Menampilkan DataFrame untuk hasil prediksi
         st.write('DataFrame untuk Hasil Prediksi:')
