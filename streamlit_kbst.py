ece7c8bad9c0cd26efd6829a63f1a6a9ad91f6f5
Rizky Yaomal Malik <2010512081@mahasiswa.upnvj.ac.id>
2024-04-16 15:01:28 +0700

    final sementara

--- streamlit_kbst.py
+++ streamlit_kbst.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import pickle
-import plotly.express as px
+#import plotly.express as px
 
 # Membaca model
 kbst_model = pickle.load(open('kbst_model.sav', 'rb'))
@@ -39,6 +39,6 @@ if uploaded_file is not None:
         st.write(merged_df)
 
 # Pie chart
-st.subheader('Pie Chart Hasil Prediksi')
-fig_pie = px.pie(hasil, names='Hasil Prediksi', title='Sebaran Hasil Prediksi')
-st.plotly_chart(fig_pie)
+#st.subheader('Pie Chart Hasil Prediksi')
+#fig_pie = px.pie(hasil, names='Hasil Prediksi', title='Sebaran Hasil Prediksi')
+#st.plotly_chart(fig_pie)
