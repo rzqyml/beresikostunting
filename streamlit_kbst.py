1c8dafd24c7d23fdbf51c149050a2dd63b20ae17
Rizky Yaomal Malik <2010512081@mahasiswa.upnvj.ac.id>
2024-04-18 21:13:50 +0700

    alhamdulillah revisi done

--- streamlit_kbst.py
+++ streamlit_kbst.py
@@ -10,20 +10,14 @@ kbst_model = pickle.load(open('kbst_model.sav', 'rb'))
 st.title('SISTEM PREDIKSI KELUARGA BERESIKO STUNTING')
 
 # Upload file CSV
-uploaded_file = st.file_uploader("Unggah file xlsx", type=["xlsx","xls"])
+uploaded_file = st.file_uploader("Unggah File Exel", type=["xlsx","xls"])
 
-# DataFrame untuk data dari file CSV
+# DataFrame untuk data dari file exce,
 if uploaded_file is not None:
     df = pd.read_excel(uploaded_file)
-    # Membaca file CSV dengan koma sebagai pemisah
-    #df_comma = pd.read_csv(uploaded_file, sep=',')
-
-    # Membaca file CSV dengan titik koma sebagai pemisah
-   # df_semicolon = pd.read_csv(uploaded_file, sep=';')
-
 
     # Menampilkan DataFrame
-    st.write('DataFrame dari File xlsx:')
+    st.write('DataFrame dari File Excel')
     st.write(df)
 
     # Tombol untuk prediksi
@@ -34,22 +28,15 @@ if uploaded_file is not None:
         # Menyimpan hasil prediksi ke dalam DataFrame hasil
         hasil = pd.DataFrame(kbst_prediction, columns=['hasil prediksi'])
 
-        # Menampilkan DataFrame untuk hasil prediksi
-        #st.write('DataFrame Hasil Prediksi:')
-        #st.write(hasil)
  # Menggabungkan dataframe hasil prediksi dengan dataframe asli
         merged_df = pd.concat([df, hasil], axis=1)
-
+        
         # Menampilkan dataframe gabungan
-        st.write('DataFrame Final:')
+        st.write('DataFrame Hasil Prediksi:')
         st.write(merged_df)
+        
  # Generate pie chart
         prediction_counts = merged_df['hasil prediksi'].value_counts()
         prediction_counts.index = ['Tidak Beresiko Stunting' if idx == 0 else 'Beresiko Stunting' for idx in prediction_counts.index]
-        fig = px.pie(prediction_counts, values=prediction_counts.values, names=prediction_counts.index,
-                     title='Prediction Distribution')
+        fig = px.pie(prediction_counts, values=prediction_counts.values, names=prediction_counts.index, title='Prediction Distribution')
         st.plotly_chart(fig)
-# Pie chart
-    # st.subheader('Pie Chart Hasil Prediksi')
-    # fig_pie = px.pie(hasil, names='Hasil Prediksi', title='Sebaran Hasil Prediksi')
-    # st.plotly_chart(fig_pie)
